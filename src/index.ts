0e7776319d26b36ab39ff8e8f5f30bf5601354fa
VincentJonathan <vinjongm@gmail.com>
2020-06-13 05:45:38 +0700

    Add worker thread option #12

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,11 @@ import {
 	Playlist,
 	PlaylistDetailed,
 	Channel,
-	SearchOptions
+	Options,
+	SearchOptions,
+	GetRelatedOptions,
 } from "./common/types";
+import { Worker } from "worker_threads";
 
 const request = bent("string");
 const url = "https://www.youtube.com/";
@@ -19,73 +22,158 @@ const searchType = {
 
 export * from "./common/types";
 
+/**
+ * Start worker for scrapping
+ * 
+ * @param scraper What to scrape
+ * @param html the HTML string
+ * @param options Scrape options
+ */
+const scrapeWorker = (scraper: string, html: string, options: Options|SearchOptions|GetRelatedOptions): Promise<any> => {
+	return new Promise(function(resolve, reject) {
+		const worker = new Worker(__dirname + "/common/worker.js", {
+			workerData: {
+				scraper: scraper,
+				html: html,
+				options: options
+			}
+		});
+
+		worker.on("message", resolve);
+		worker.on("error", reject);
+	});
+};
+
 
 /**
  * Search youtube for a list of  based on a search query.
+ * 
  * @param query Search Query
  * @param options (optional) Option for search type and limit
  */
 const search = async (query: string, options: SearchOptions={}): Promise<(Video|Channel|Playlist)[]> => {
+	if (query.trim().length === 0) throw(new Error("Query cannot be blank"));
+
 	if (options === undefined) options = {};
 	options = {
 		type: "video",
+		useWorkerThread: false, 
 		limit: 10,
 		page: 1,
 		...options
 	};
+
 	let searchUrl = url + "results?";
-	if (query.trim().length === 0) throw(new Error("Query cannot be blank"));
 	if (options.type && searchType[options.type]) searchUrl += `sp=${searchType[options.type]}&`;
 	else searchUrl += `sp=${searchType["video"]}&`; //Default type will be video
 	searchUrl += `page=${options.page}&search_query=${query}`;
 		
 	const html = await request(searchUrl);
-	return ps.parseSearch(html, options);
+	
+	if (options.useWorkerThread) {
+		return await scrapeWorker("search", html, options);
+	} else {
+		return ps.parseSearch(html, options);
+	}
 };
 
 /**
  * Search youtube for playlist information.
+ * 
  * @param playlistId Id of the playlist
  */
-const getPlaylist = async (playlistId: string): Promise<PlaylistDetailed|{}> => {
+const getPlaylist = async (playlistId: string, options: Options={}): Promise<PlaylistDetailed|{}> => {
 	if (playlistId.trim().length === 0) throw (new Error("Playlist ID cannot be blank"));
+
+	if (options === undefined) options = {};
+	options = {
+		useWorkerThread: false, 
+		...options
+	};
+
 	const playlistUrl = `${url}playlist?list=${playlistId}`;
 	const html = await request(playlistUrl);
-	return ps.parseGetPlaylist(html);
+
+	if (options.useWorkerThread) {
+		return await scrapeWorker("getPlaylist", html, options);
+	} else {
+		return ps.parseGetPlaylist(html);
+	}
+
 };
 
 /**
  * Search youtube for video information.
+ * 
  * @param videoId Id of the video
  */
-const getVideo = async (videoId: string): Promise<VideoDetailed|{}> => {
+const getVideo = async (videoId: string, options: Options): Promise<VideoDetailed|{}> => {
 	if (videoId.trim().length === 0) throw(new Error("Video ID cannot be blank"));
+
+	if (options === undefined) options = {};
+	options = {
+		useWorkerThread: false, 
+		...options
+	};
+
 	const videoUrl = `${url}watch?v=${videoId}`;
 	const html = await request(videoUrl);
-	return ps.parseGetVideo(html);
+
+	if (options.useWorkerThread) {
+		return await scrapeWorker("getVideo", html, options);
+	} else {
+		return ps.parseGetVideo(html);
+	}
 };
 
 /**
- * Search youtube for related videos based on videoId .
+ * Search youtube for related videos based on videoId.
+ * 
  * @param videoId Id of the video
  * @param limit (optional) Max videos count
  */
-const getRelated = async (videoId: string, limit = 10): Promise<Video[]> => {
+const getRelated = async (videoId: string, options: GetRelatedOptions): Promise<Video[]> => {
 	if (videoId.trim().length === 0) throw(new Error("Video ID cannot be blank"));
+
+	if (options === undefined) options = {};
+	options = {
+		useWorkerThread: false,
+		limit: 10,
+		...options
+	};
+
 	const videoUrl = `${url}watch?v=${videoId}`;
 	const html = await request(videoUrl);
-	return ps.parseGetRelated(html, limit);
+
+	if (options.useWorkerThread) {
+		return await scrapeWorker("getRelated", html, options);
+	} else {
+		return ps.parseGetRelated(html, options.limit || 10);
+	}
 };
 
 /**
  * Search youtube for up next video based on videoId.
+ * 
  * @param videoId Id of the video
  */
-const getUpNext = async (videoId: string): Promise<Video|{}> => {
+const getUpNext = async (videoId: string, options: Options): Promise<Video|{}> => {
 	if (videoId.trim().length === 0) throw(new Error("Video ID cannot be blank"));
+
+	if (options === undefined) options = {};
+	options = {
+		useWorkerThread: false, 
+		...options
+	};
+
 	const videoUrl = `${url}watch?v=${videoId}`;
 	const html = await request(videoUrl);
-	return ps.parseGetUpNext(html);
+
+	if (options.useWorkerThread) {
+		return await scrapeWorker("getUpNext", html, options);
+	} else {
+		return ps.parseGetUpNext(html);
+	}
 };
 
 
